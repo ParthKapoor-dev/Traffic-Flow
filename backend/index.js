f8a3523e329554f35ad0dcde7511b98fcf2f544e
Parth Kapoor <pkapoor_be22@thapar.edu>
2024-07-26 20:08:16 +0530

    minor changes:

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded(true));
 
-app.listen(process.env.PORT, () => {
+console.log(process.env.PORT);
+
+app.listen(process.env.PORT || 5000, () => {
   mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error(err));
